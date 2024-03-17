3709b436ad240127eb3e5be6cf1f1946a721db5f
Bruno <brunopivaroc05@gmail.com>
2024-03-17 18:08:22 -0300

    adicionando as bibliotecas

---
+++ ml_miniprojeto2.py
@@ -0,0 +1,12 @@
+#Importando bibliotecas
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+import seaborn as sns
+import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import Lasso
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score, explained_variance_score
